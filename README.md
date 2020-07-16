68e50bce66931eee0fe5e54b617c68d0ebc59e8d
Eddie Cheng <local@example.test>
2020-07-16 12:04:08 +0200

    first set

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# React_useState_tutorial
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // useState returns array!
+  // 10 == initial state
+  // setCount == function
+  // count == value
+  const [{count1, count2}, setCount] = useState({count1:10, count2:100});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <button onClick={() => setCount(c => ({
+        ...c,
+        count1:c.count1++
+      }))}>
+        +
+      </button>
+      <div>{count1}</div>
+      <div>{count2}</div>
+    </div>);
 }
 
 export default App;
